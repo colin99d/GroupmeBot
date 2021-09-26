d09ae84e8d58f90ad2e1bb4e3558e7c635a3c375
Colin Delahunty <colindelahunty@Colins-Air.indiana.edu>
2021-09-25 22:16:27 -0400

    Added gunicorn

--- bot/views.py
+++ bot/views.py
@@ -16,9 +16,15 @@ def handler(request):
     if request.method == "POST":
         body = json.loads(request.body.decode('utf-8'))
         name = body.get("name").strip()
+        text = body.get("text").strip()
 
-        if name not in bot_names:
-            send_message("Passed")
+        if name not in bot_names and ("@SportsBot" in text or "@TestBot" in text):
+            if "help" in text.lower():
+                send_message("Options:\n-Johnny\n-Fantasy")
+            elif "johnny" in text.lower():
+                send_message("Johnny its been years, reproduce already")
+            elif "fantasy" in text.lower():
+                send_message("Stop the steal! The commish allows collusion!!!")
             return HttpResponse(status=200)
     else:
         http_resp = f"<p>The page is working.</p>"

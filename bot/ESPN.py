938d246d4212c02872a4727de084e9f306d1d6f8
Colin Delahunty <colindelahunty@Colins-MacBook-Air.local>
2021-09-28 09:05:56 -0400

    Added scores option

---
+++ bot/ESPN.py
@@ -0,0 +1,27 @@
+import requests
+
+#"&view=mDraftDetail&view=mLiveScoring&view=mMatchupScore&view=mPendingTransactions&view=mPositionalRatings&view=mSettings&view=modular&view=mNav&view=mMatchupScore"
+
+
+def get_standings():
+    base = "http://fantasy.espn.com/apis/v3/games/ffl/seasons/"
+    year = "2021"
+    mid = "/segments/0/leagues/"
+    leagueID = "1659909443"
+    tail = "?view=mTeam"
+    url = f"{base}{year}{mid}{leagueID}{tail}"
+
+    r = requests.get(url,
+                 cookies={"SWID": "{01E541AA-E88F-4CFF-9978-CB5C2AB064BB}",
+                          "espn_s2": "AECxO0pAyX7P4f55fN10Cf%2FgVHdoGXTkAlYQBxI4WBN0vYzOFipvz0zvt6gwIKkgpcVJK0CsCLf5tE1kNxzBNy%2FnNbZ3t9KnQjc8xCHrecZiOAr3zGU569IpRGGqIDZjzOF%2BKhTmDJbyI%2B7zDrTU2Bqs3C4uuDzvWtWnVq%2FgIHNUJ%2FvRiIc3bHdVRbBcY5s3ffi9mHTkZJiQruqNHggRyQjCfS8fv%2FQibP31tkkCx%2FtGmNHn2GOPFvjiuye4F%2B4STtdP9TnmGWN5D7HhCLdBvnqYp5KlPzj%2FwoSIwFdqH8qopg%3D%3D" })
+    d = r.json()["teams"]
+    r.status_code
+
+    d.sort(key=lambda x: x['record']['overall']['wins'], reverse=True)
+
+    text = 'Name\tWins\tLosses\n'
+
+    for t in d:
+        text += f"{t['abbrev']}\t{t['record']['overall']['wins']}\t{t['record']['overall']['losses']}\n"
+
+    return text
--- bot/views.py
+++ bot/views.py
@@ -1,7 +1,10 @@
+import json
+
+import requests
 from django.views.decorators.csrf import csrf_exempt
 from django.http import HttpResponse
-import requests
-import json
+
+from ESPN import get_standings
 
 bot_names = ["SportsBot", "TestBot"]
 
@@ -28,18 +31,21 @@ def handler(request):
     if request.method == "POST":
         body = json.loads(request.body.decode('utf-8'))
         name = body.get("name").strip()
-        text = body.get("text").strip()
+        text = body.get("text").strip().lower()
         group_id = body.get("group_id").strip()
         if name not in bot_names:
-            if "retard" in text.lower():
+            if "retard" in text:
                 send_message("R-word hurts!!!", group_id)
             if "@SportsBot" in text or "@TestBot" in text:
-                if "help" in text.lower():
-                    send_message("Options:\n-Johnny\n-Fantasy\n\nAnd NO saying the r-word!", group_id)
-                elif "johnny" in text.lower():
+                if "help" in text:
+                    send_message("Options:\n-Johnny\n-Fantasy\n-Scores\n\nAnd NO saying the r-word!", group_id)
+                elif "johnny" in text:
                     send_message("Johnny its been years, reproduce already", group_id)
-                elif "fantasy" in text.lower():
+                elif "fantasy" in text:
                     send_message("Stop the steal! The commish allows collusion!!!", group_id)
+                elif "scores" in text:
+                    message = get_standings()
+                    send_message(message, group_id)
                 return HttpResponse(status=200)
     else:
         http_resp = f"<p>The page is working.</p>"

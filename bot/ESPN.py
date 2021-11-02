f928ddb75ea16862f01c842e848924fd1e9ed1f3
Colin Delahunty <colindelahunty@Colins-Air.indiana.edu>
2021-11-01 21:36:52 -0400

    Modified winner function

--- bot/ESPN.py
+++ bot/ESPN.py
@@ -48,6 +48,7 @@ def get_standings() -> str:
 
 def win_chance() -> str:
     d = team_stats()
+    total = d[0]["record"]["overall"]["losses"] + d[0]["record"]["overall"]["wins"]
     plyrs = []
     t_pts = 0
     t_wins = 0
@@ -58,10 +59,15 @@ def win_chance() -> str:
         t_wins += ws
         plyrs.append([p["abbrev"], pts, ws])
 
-    players_cln = [
-        [plr[0], round((plr[1] / t_pts * 0.5 + plr[2] / t_wins * 0.5) * 100, 2)]
-        for plr in plyrs
-    ]
+    if total > 9:
+        return f"Winner is: {max(plyrs, key=lambda x: x[2])[0]}"
+    most_wins = max(plyrs, key=lambda x: x[2])[2]
+    games_left = 10 - total
+    plyrs = list(filter(lambda x: x[2] + games_left >= most_wins, plyrs))
+
+    plyrs_cln = [[x[0], (x[1] / t_pts * 0.5 + x[2] / t_wins * 0.5)] for x in plyrs]
+    total_pts = sum(x[1] for x in plyrs_cln)
+    players_cln = [[x[0], round((x[1] / total_pts) * 100, 2)] for x in plyrs_cln]
     players_cln.sort(key=lambda x: x[1], reverse=True)
 
     string = "Player's chance of winning:"
@@ -70,3 +76,6 @@ def win_chance() -> str:
         string += f"\n{plr[0]} {gap}{plr[1]}%"
 
     return string
+
+
+print(win_chance())

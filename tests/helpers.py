01c2f1d9f5b7db4ad30ae62605766a84041349bd
colin99d <colin99delahunty@gmail.com>
2022-07-23 18:20:41 -0400

    Switched from unittest to pytest

--- tests/helpers.py
+++ tests/helpers.py
@@ -1,36 +1,3 @@
-import functools
-import io
-import sys
-
-
-def check_print(assert_in: str = "", length: int = -1):
-    """Captures output of print function and checks if the function contains a given string"""
-
-    def checker(func):
-        @functools.wraps(func)
-        def wrapper(*args, **kwargs):
-            capturedOutput = io.StringIO()
-            sys.stdout = capturedOutput
-            func(*args, **kwargs)
-            sys.stdout = sys.__stdout__
-            capt = capturedOutput.getvalue()
-            if assert_in:
-                assert assert_in in capt
-                return None
-            if length >= 0:
-                assert len(capt) > length
-            return capt
-
-        return wrapper
-
-    return checker
-
-
-def return_vals(*args, **_):
-    for arg in args:
-        print(arg)
-
-
 def get_post(message):
     return {
         "attachments": [],
--- tests/test_functional.py
+++
@@ -1,2 +0,0 @@
-"""test_integration"""
-__docformat__ = "numpy"
